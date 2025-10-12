13f0defdb608cb9b7bf77009c80701c56cdfd258
ArunVKammath <arun@snaptrude.com>
2025-10-13 00:44:44 +0530

    Using semantic tags

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,8 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">//Start the Project here</div>;
+  return (
+    <header style={{ textAlign: "center" }}>Start the Project here</header>
+  );
 }
 
 export default App;
