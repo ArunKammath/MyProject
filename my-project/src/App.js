2b23be034b0db5dc0e004ae80d5a9a3b8daac17f
ArunVKammath <arun@snaptrude.com>
2025-10-13 12:11:35 +0530

    Adding CSS to starting Page

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -2,7 +2,23 @@ import "./App.css";
 
 function App() {
   return (
-    <header style={{ textAlign: "center" }}>Start the Project here</header>
+    <body id="pageLayout">
+      <nav id="navigation">
+        <h1>Navigation Bar</h1>
+      </nav>
+      <header id="heroSection">
+        <h1>Hero Section</h1>
+      </header>
+      <main id="highlights">
+        <h1>Highlights</h1>
+      </main>
+      <header id="testimonials">
+        <h1>Testimonials</h1>
+      </header>
+      <footer id="footer">
+        <h1>Footer</h1>
+      </footer>
+    </body>
   );
 }
 
