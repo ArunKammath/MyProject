27d761c8bb28dd365eff6bfc96cf4129fecb3fe9
ArunVKammath <arunvenugopalkammath97@gmail.com>
2025-10-28 12:40:44 +0530

    removing Erros in console

--- my-project/src/components/main.js
+++ my-project/src/components/main.js
@@ -48,13 +48,13 @@ function TopNav() {
 function HeroSection() {
   return (
     <header id="heroSection">
-      <content>
+      <section>
         <h1>Tattukkada</h1>
         <h2>Kochi</h2>
         <p>Authntic Kerala Food with a mix of traditional and modern cuisine</p>
         <p>Open from 10:00 AM to 12:00 pm and 4pm to 10pm</p>
         <button>Reserve a table</button>
-      </content>
+      </section>
       <img src={snacksImage} alt="snacksImage" />
     </header>
   );
@@ -63,26 +63,26 @@ function HeroSection() {
 function Main() {
   return (
     <main id="highlights">
-      <heading>
+      <header>
         <h1>Specials</h1>
         <button>Online Menu</button>
-      </heading>
-      <section>
-        <special1>
+      </header>
+      <section id="specials">
+        <section id="elanji">
           <img src={Elanji} alt="Elanji" />
           <h1>Elanji</h1>
           <p>A mix of grated coconut and dryfruits!</p>
-        </special1>
-        <special2>
+        </section>
+        <section id="kaypola">
           <img src={kaypola} alt="Kaypola" />
           <h1>Kaypola</h1>
           <p>Fruits in an Egg cake!</p>
-        </special2>
-        <special3>
+        </section>
+        <section id="ullivada">
           <img src={ullivada} alt="Ullivada" />
           <h1>Ullivada</h1>
           <p>Onion with batter deep fried!</p>
-        </special3>
+        </section>
       </section>
     </main>
   );
@@ -100,22 +100,22 @@ function Testimonials() {
   return (
     <header id="testimonials">
       <h1>Testimonials</h1>
-      <section>
-        <section1>
+      <section id="rating">
+        <section>
           <Star num={5} />
           <h1>Ranchal</h1>
           <p>Kaypola or nothing!</p>
-        </section1>
-        <section2>
+        </section>
+        <section>
           <Star num={5} />
           <h1>Atul</h1>
           <p>Chai se pyaar hai!</p>
-        </section2>
-        <section3>
+        </section>
+        <section>
           <Star num={5} />
           <h1>Kammath</h1>
           <p>Ullivada the masterpiece!</p>
-        </section3>
+        </section>
       </section>
     </header>
   );
@@ -145,46 +145,20 @@ function Login() {
   return <div>Login</div>;
 }
 
-function Reservations() {
-  return (
-    <form id="reservations">
-      <label htmlFor="res-date">Choose date</label>
-      <input type="date" id="res-date" />
-      <label htmlFor="res-time">Choose time</label>
-      <select id="res-time ">
-        <option value="17:00">17:00</option>
-        <option value="18:00">18:00</option>
-        <option value="19:00">19:00</option>
-        <option value="20:00">20:00</option>
-        <option value="21:00">21:00</option>
-        <option value="22:00">22:00</option>
-      </select>
-      <label htmlFor="guests">Number of guests</label>
-      <input type="number" placeholder="1" min="1" max="10" id="guests" />
-      <label htmlFor="occasion">Occasion</label>
-      <select id="occasion">
-        <option>Birthday</option>
-        <option>Anniversary</option>
-      </select>
-      <input type="submit" value="Make Your reservation" />
-    </form>
-  );
-}
-
 function OrderOnline() {
   return <div>OrderOnline</div>;
 }
 
 function Contact() {
   return (
-    <content id="footer">
+    <section id="footer">
       <img src={logo} alt="logo" />
       <p>Phone: 9876543210</p>
       <p>Email: tattukkada@gmail.com</p>
       <p>Address: somewhere, Kochi</p>
       <p>Copyright @ 2025 Tattukkada</p>
-    </content>
+    </section>
   );
 }
 
-export { TopNav, HomePage, Login, About, Reservations, OrderOnline, Contact };
+export { TopNav, HomePage, Login, About, OrderOnline, Contact };
