5a3e6c1c29ec3c6e30c6ca6907955180076ce53c
ArunVKammath <arunvenugopalkammath97@gmail.com>
2025-10-28 11:49:33 +0530

    adding guest number in reservation

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,4 +1,5 @@
-import "./App.css";
+import "./style/App.css";
+import "./style/reservation.css"
 import { Routes, Route } from "react-router-dom";
 import {
   TopNav,
--- my-project/src/components/reservation.js
+++ my-project/src/components/reservation.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
 
 
+
 function Reservations() {
     const [booking, setBooking] = useState({
         date: "",
         time: "",
+        numGuests:"",
         occasion: "",
     });
 
@@ -22,30 +24,37 @@ function Reservations() {
         setBooking({
             date: "",
             time: "",
+            numGuests:"",
             occasion: "",
         });
     };
   return (
     <form id="reservations" onSubmit={handleSubmit}>
-      <label htmlFor="res-date">Choose date</label>
-      <input type="date" id="res-date" name="date" value={booking.date} onChange={handleChange} />
-      <label htmlFor="res-time">Choose time</label>
-      <select id="res-time" name="time" value={booking.time} onChange={handleChange}>
-        <option value="">Select a time</option>
-        <option value="17:00">17:00</option>
-        <option value="18:00">18:00</option>
-        <option value="19:00">19:00</option>
-        <option value="20:00">20:00</option>
-        <option value="21:00">21:00</option>
-        <option value="22:00">22:00</option>
-      </select>
-      <label htmlFor="occasion">Occasion</label>
-      <select id="occasion" name="occasion" value={booking.occasion} onChange={handleChange}>
-        <option value="">Select an occasion</option>
-        <option value="Birthday">Birthday</option>
-        <option value="Anniversary">Anniversary</option>
-      </select>
-      <input type="submit" value="Make Your reservation" />  
+      <label htmlFor="res-date" style={{ fontSize: "20px"}}>Reservation date 
+        <input type="date" id="res-date" name="date" value={booking.date} onChange={handleChange} />
+      </label>
+        <label htmlFor="res-time" style={{ fontSize: "20px" }}>Reservation time 
+        <select id="res-time" name="time" value={booking.time} onChange={handleChange}>
+          <option value="">Select a time</option>
+          <option value="17:00">17:00</option>
+          <option value="18:00">18:00</option>
+          <option value="19:00">19:00</option>
+          <option value="20:00">20:00</option>
+          <option value="21:00">21:00</option>
+          <option value="22:00">22:00</option>
+        </select>
+      </label>
+      <label>Number of guests:
+          <input type="number" placeholder="1"/>
+      </label>
+      <label htmlFor="occasion" style={{ fontSize: "20px" }}>Occasion :  
+        <select id="occasion" name="Celebration" value={booking.occasion} onChange={handleChange}>
+          <option value="">Select an occasion</option>
+          <option value="Birthday">Birthday</option>
+          <option value="Anniversary">Anniversary</option>
+        </select>
+      </label>
+      <input id="submit" type="submit" value="Make Your reservation" />  
     </form>
   );
 }
