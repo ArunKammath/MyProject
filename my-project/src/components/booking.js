88753076b5a127fe1cd05271528b122325d39fbd
ArunVKammath <arunvenugopalkammath97@gmail.com>
2025-10-31 11:49:31 +0530

    using context for booking list

--- my-project/src/components/booking.js
+++ my-project/src/components/booking.js
@@ -2,7 +2,7 @@ import { createContext, useState, useContext } from "react";
 
 const bookingContext = createContext(undefined);
 
-export const  bookingProvider =({children})=>{
+export const BookingProvider = ({ children }) => {
     const [bookingList, setBookingList] = useState([]);
 
     return (<bookingContext.Provider value={{bookingList, setBookingList}}>
