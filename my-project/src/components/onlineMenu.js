a8e9bcd23c9d4786474832506aa98d04ab602a7c
ArunVKammath <arunvenugopalkammath97@gmail.com>
2025-11-10 16:27:52 +0530

    Finishing styling to online menu

--- my-project/src/components/onlineMenu.js
+++ my-project/src/components/onlineMenu.js
@@ -14,50 +14,56 @@ function OrderOnline() {
                 <header>
                     <h1>Beverages</h1>
                 </header>
-                <section id="tea">
-                    <h1>Tea</h1>
-                    <img src={tea} alt="tea" />
-                    <h2>Rs. 15.00 </h2>
+                <section id="beverageList">
+                    <section id="tea">
+                        <h1>Tea</h1>
+                        <img src={tea} alt="tea" />
+                        <h2>Rs. 15.00 </h2>
+                    </section>
+                    <section id="coffee">   
+                        <h1>Coffee</h1>
+                        <img src={coffee} alt="coffee" />
+                        <h2>Rs. 20.00 </h2>
                 </section>
-                <section id="coffee">   
-                    <h1>Coffee</h1>
-                    <img src={coffee} alt="coffee" />
-                    <h2>Rs. 20.00 </h2>
                 </section>
             </section>
             <section id="snacks">
                 <header>
                     <h1>Snacks</h1>
                 </header>
-                <section id="elanji">
-                    <h1>Elanji</h1>
-                    <img src={Elanji} alt="elanji" />
-                    <h2>Rs. 30.00 </h2>
-                </section>
-                <section id="kaypola">
-                    <h1>Kaypola</h1>
-                    <img src={kaypola} alt="kaypola" />
-                    <h2>Rs. 40.00 </h2>
-                </section>
-                <section id="ullivada">
-                    <h1>Ullivada</h1>
-                    <img src={ullivada} alt="ullivada" />
-                    <h2>Rs. 50.00 </h2>
+                <section id="snacksList">
+                    <section id="elanji">
+                        <h1>Elanji</h1>
+                        <img src={Elanji} alt="elanji" />
+                        <h2>Rs. 30.00 </h2>
+                    </section>
+                    <section id="kaypola">
+                        <h1>Kaypola</h1>
+                        <img src={kaypola} alt="kaypola" />
+                        <h2>Rs. 40.00 </h2>
+                    </section>
+                    <section id="ullivada">
+                        <h1>Ullivada</h1>
+                        <img src={ullivada} alt="ullivada" />
+                        <h2>Rs. 50.00 </h2>
+                    </section>
                 </section>
             </section>
             <section id="desserts">
                 <header>
                     <h1>Desserts</h1>
                 </header>
-                <section id="tenderCocnut">
-                    <h1>Tender Coconut Pudding</h1>
-                    <img src={tenderCoconut} alt="tenderCoconut" />
-                    <h2>Rs. 40.00 </h2>
-                </section>
-                <section id="banana Halwa">
-                    <h1>Banana Halwa</h1>
-                    <img src={bananaHalwa} alt="bananaHalwa" />
-                    <h2>Rs. 50.00 </h2>
+                <section id="dessertsList">
+                    <section id="tenderCocnut">
+                        <h1>Tender Coconut Pudding</h1>
+                        <img src={tenderCoconut} alt="tenderCoconut" />
+                        <h2>Rs. 40.00 </h2>
+                    </section>
+                    <section id="bananaHalwa">
+                        <h1>Banana Halwa</h1>
+                        <img src={bananaHalwa} alt="bananaHalwa" />
+                        <h2>Rs. 50.00 </h2>
+                    </section>
                 </section>
             </section>
         </React.Fragment>
